dbb6a7ca8fb7fa73e94c394e3f3e4937bff0ad46
qingAry <1605880839@qq.com>
2020-04-27 18:21:28 +0800

    antd

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import { Button } from 'antd';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <h3>这是app组件</h3>
+        <h2>玉晴_app组件</h2><br/>
+        <Button type="primary">点击按钮</Button>
       </div>
     )
   }
