ea887d736ec9c6635907b15ca88d9b6aeea8fcef
qingAry <1605880839@qq.com>
2020-04-28 17:58:38 +0800

    更新

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react'
 import {Route,Switch,Redirect} from 'react-router-dom'
-import Login from './components/Login/Login'
-import Admin from './components/Admin/Admin'
+import Login from './pages/Login/Login'
+import Admin from './pages/Admin/Admin'
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <h3>这是三级标题</h3>
         <Switch>
           <Route path="/login" component={Login}/>
           <Route path="/admin" component={Admin}/>
